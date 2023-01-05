9cd9be7cfe9c386fd8ec674a1a82ec66dcb76880
Prasenjit Purohit <prasenjit@prasenjit.net>
2023-01-05 12:21:52 -0800

    annotation imported

--- src/main/java/com/example/interview/controller/AccountController.java
+++ src/main/java/com/example/interview/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.example.interview.model.AccountRequest;
 import com.example.interview.model.AccountResponse;
 import com.example.interview.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
